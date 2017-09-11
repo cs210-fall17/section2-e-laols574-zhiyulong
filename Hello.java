6c519bf890210425a760f6e0973e3989691eed27
Lauren Olson <lauren_olson@dhcp-10-134-204-155.uawifi.arizona.edu>
2017-09-11 16:57:16 -0700

    more

--- Hello.java
+++ Hello.java
@@ -9,28 +9,19 @@ public class Hello {
     }
 
     public static void callSign() {
-        
-<<<<<<< HEAD
-        System.out.println("I copy, gold leader!" "Lock S-foils in attack position." );
-        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-=======
-        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
-	System.out.println(“Stabilize your rear”)
->>>>>>> 95373e03b3e2aeafb34fefcb32cf78b1bd908de0
+        System.out.println("I copy, gold leader! Lock S-foils in attack position. Stabilize your rear.");
+
 
     }
 
     public static void goldLeader() {
-
-	System.out.println(“Loosen up!”)
+		System.out.println("Loosen up!");
     
 
     }
 
     public static void redLeader() {
- 
-         
-          System.out.println("I copy, gold leader!");
+		 System.out.println("I copy, gold leader!");
 
         
 
